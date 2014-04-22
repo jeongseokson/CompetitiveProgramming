b832867368069188400a91053a2fd1e052d93e23
Jeongseok Son <invictusjs@gmail.com>
2014-04-22 22:17:01 +0900

    Fix an error in bit manipulation snippet
    Removed a wrong parameter in lowBit macro.

--- snippets/bit_manipulation.cpp
+++ snippets/bit_manipulation.cpp
@@ -11,7 +11,7 @@
 #define setBit(S, j) ((S) |= (1 << (j)))
 #define clearBit(S, j) ((S) &= ~(1 << (j)))
 #define toggleBit(S, j) ((S) ^= (1 << (j)))
-#define lowBit(S, j) ((S) & -(S))
+#define lowBit(S) ((S) & -(S))
 #define setAll(S, n) ((S) = (1 << (n)) - 1)
 
 #define modulo(S, N) ((S) & (N - 1)) // where N is a power of 2
