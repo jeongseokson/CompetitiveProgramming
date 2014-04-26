b1056aa8e3a8e38c2df253cc4822cf781c246d64
Jeongseok Son <invictusjs@gmail.com>
2014-04-26 18:16:18 +0900

    Add tarjan algorithm to dfs applicatio snippet
    Also removed unnecessary cross edge variable.

--- snippets/dfs_applications.cpp
+++ snippets/dfs_applications.cpp
@@ -49,7 +49,6 @@ void test_topological_sort() {
 #define DFS_GRAY 2
 vi dfs_parent;
 vii back_edge;
-vii cross_edge;
 
 void graph_check(int u) {
     dfs_num[u] = DFS_GRAY;
@@ -62,6 +61,7 @@ void graph_check(int u) {
             if (v.first != dfs_parent[u])
                 back_edge.push_back(ii(u, v.first));
         } else if (dfs_num[v.first] == DFS_BLACK) {
+            // cross edge or forward edge
         }
     }
     dfs_num[u] = DFS_BLACK;
@@ -76,9 +76,99 @@ void test_dfs_tree_check() {
     assert(equal(back_edge.begin(), back_edge.end(), back_edge_arr));
 }
 
+vi dfs_low;
+vi articulation_vertex;
+int dfs_number_counter, dfs_root, root_children;
+vii bridge;
+
+void find_articulation_point(int u) {
+    dfs_low[u] = dfs_num[u] = dfs_number_counter++;
+    for (int j = 0; j < AdjList[u].size(); j++) {
+        ii v = AdjList[u][j];
+        if (dfs_num[v.first] == DFS_WHITE) {
+            dfs_parent[v.first] = u;
+            if (u == dfs_root)
+                root_children++;
+            find_articulation_point(v.first);
+            if (dfs_low[v.first] >= dfs_num[u])
+                articulation_vertex[u] = true;
+            if (dfs_low[v.first] > dfs_num[u])
+                bridge.push_back(ii(u, v.first));
+            dfs_low[u] = min(dfs_low[u], dfs_low[v.first]);
+        } else if (v.first != dfs_parent[u]) {
+            dfs_low[u] = min(dfs_low[u], dfs_num[v.first]);
+        }
+    }
+}
+
+void test_articulation_points_and_bridges() {
+    int articulation_vertex_arr[] = {0, 1, 0, 1, 0, 0, 1, 0, 0};
+    ii bridge_arr[] = {ii(0, 1), ii(3, 4), ii(6, 7), ii(6, 8)};
+
+    dfs_number_counter = 0;
+    dfs_num.assign(V, DFS_WHITE);
+    dfs_low.assign(V, 0);
+    dfs_parent.assign(V, -1);
+    articulation_vertex.assign(V, 0);
+
+    for (int i = 0; i < V; i++) {
+        dfs_root = i; root_children = 0;
+        if (dfs_num[i] == DFS_WHITE) {
+            find_articulation_point(i);
+            articulation_vertex[dfs_root] = (root_children > 1);
+        }
+    }
+
+    assert(equal(articulation_vertex.begin(), articulation_vertex.end(),
+                articulation_vertex_arr));
+
+    sort(bridge.begin(), bridge.end());
+    assert(equal(bridge.begin(), bridge.end(), bridge_arr));
+} 
+
+vi S, visited;
+int num_scc;
+vector<vi> scc;
+
+void tarjan_scc(int u) {
+    dfs_low[u] = dfs_num[u] = dfs_number_counter++;
+    S.push_back(u);
+    visited[u] = 1;
+    for (int j = 0; j < (int)AdjList[u].size(); j++) {
+        ii v = AdjList[u][j];
+        if (dfs_num[v.first] == DFS_WHITE)
+            tarjan_scc(v.first);
+        if (visited[v.first])
+            dfs_low[u] = min(dfs_low[u], dfs_low[v.first]);
+    }
+
+    if (dfs_low[u] == dfs_num[u]) {
+        while (1) {
+            int v = S.back(); S.pop_back(); visited[v] = 0;
+            if (u == v)
+                break;
+        }
+        ++num_scc;
+    }
+
+}
+
+void test_scc() {
+    dfs_num.assign(V, DFS_WHITE);
+    dfs_low.assign(V, 0);
+    visited.assign(V, 0);
+    dfs_number_counter = num_scc = 0;
+
+    for (int i = 0; i < V; i++)
+        if (dfs_num[i] == DFS_WHITE)
+            tarjan_scc(i);
+    
+    assert(num_scc == 3);
+}
+
 #define addEdge(from, to, weight) AdjList[(from)].push_back(ii((to), (weight)))
 
-void input() {
+void inputUndirected() {
     V = 9;
     AdjList.assign(V, vii());
 
@@ -92,8 +182,23 @@ void input() {
     addEdge(8, 6, 0); 
 }
 
+void inputDirected() {
+    V = 8;
+    AdjList.assign(V, vii());
+
+    addEdge(0, 1, 0);
+    addEdge(1, 3, 0); 
+    addEdge(2, 1, 0);
+    addEdge(3, 2, 0);
+    addEdge(4, 5, 0);
+    addEdge(5, 7, 0);
+    addEdge(6, 4, 0);
+    addEdge(7, 6, 0);
+}
+
+
 int main() {
-    input();
+    inputUndirected();
 
     // Topological Sort
     test_topological_sort();
@@ -102,6 +207,10 @@ int main() {
     test_dfs_tree_check();
     
     // Articulation Points & Bridges
+    test_articulation_points_and_bridges();
     
+    inputDirected();
+
     // Strongly Connected Components
+    test_scc();
 }
